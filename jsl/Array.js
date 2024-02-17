7573f84e291fa433bab764bb7fd5834c28eeb3e0
Sourabh-km13 <157295892+Sourabh-km13@users.noreply.github.com>
2024-02-17 17:19:46 +0000

    arraysdone

---
+++ jsl/Array.js
@@ -0,0 +1,11 @@
+arr=[1,2,3,4,5]
+console.log(arr)
+arr2=[7,8,4]
+//console.log(arr.concat(arr2))
+arrnest=[1,2,3,[4,3,[9,0]]]
+//console.log(arrnest.flat(Infinity))
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    console.log(element);
+    
+}
---
+++ jsl/first.js
@@ -0,0 +1,14 @@
+let a="sourabh"
+ a=null;
+/*console.log(a.length)
+console.log(a.toUpperCase())
+console.log(a.indexOf("o"))
+console.log(a.charAt(3))*/
+// console.log(typeof(a))
+// let b= new Boolean
+// b=0
+// console.log(Boolean(b))
+let c=-3.95957
+console.log(Math.floor(c))
+
+
