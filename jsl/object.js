ad7ab43657d1d8a1be7caaf3dfa1c547e89a604d
Sourabh-km13 <157295892+Sourabh-km13@users.noreply.github.com>
2024-02-18 07:24:12 +0000

    object changes

---
+++ jsl/object.js
@@ -0,0 +1,11 @@
+const obj1={}
+obj1.name="blue"
+//console.log(obj1);
+Object.freeze(obj1)
+obj1.name="black"
+//console.log(obj1);
+//rm to remove file
+//touch to create file
+const student={name:'sourabh', rollno:40}
+console.log(student);
+console.log(typeof(student.rollno));
